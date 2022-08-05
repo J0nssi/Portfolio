ac39af14f123671c8639d534b0a38e1052ceaccb
J0nssi <joonas.vaija@hotmail.com>
2022-08-05 15:53:59 +0300

    Changed about text

--- src/Components/About.js
+++ src/Components/About.js
@@ -16,7 +16,7 @@ const About = () => {
             </Fade>
             <Fade right duration={750} delay={500} distance="30px">
               <div className="rightside_about">
-                <p>It all started when I applied to Savonia UAS in 2019 and got into my studies as IT Engineer. Three years later I'm near the finish line with my studies.  
+                <p>It all started when I applied to Savonia UAS in 2019 and got into my Bachelor of Engineering, Information Technology studies. Three years later I'm near the finish line with my studies.  
                 </p>
                 <br/>
                 <p>I learned a lot but there is still a lot more to learn. That is why I'm always trying to improve and learn new things and technologies to keep me on track with latest trends.</p>
